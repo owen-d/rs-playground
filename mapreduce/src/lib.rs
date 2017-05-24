cd2208532492b4ecc6e041f57e73a1de72926459
owen-d <ow.diehl@gmail.com>
2017-05-23 20:10:19 -0400

    working on a threading example

--- mapreduce/src/lib.rs
+++ mapreduce/src/lib.rs
@@ -1,32 +1,38 @@
 extern crate futures;
-extern crate tokio_core;
 extern crate futures_cpupool;
 
-use self::futures::future::{Future, BoxFuture};
-use self::tokio_core::reactor::Core;
-use self::futures_cpupool::CpuPool;
+use futures::{future, Future, Stream};
+use future::BoxFuture;
+use futures::sync::mpsc;
+use futures_cpupool::{CpuPool, CpuFuture};
 use std::io;
 
-pub struct Delegator<T>
-{
+pub struct Delegator<T> {
     pub pool: CpuPool,
-    pub data: Option<T>,
+    pub data: Vec<Result<T, ()>>,
 }
 
 impl<T> Delegator<T>
-    where T: Send + Clone
+    where T: Send + Sync + std::ops::Add<Output=T>
 {
-    pub fn new() -> io::Result<Self> {
+    pub fn new(data: Vec<T>) -> io::Result<Self> {
         Ok(Delegator {
             pool: CpuPool::new_num_cpus(),
-            data: None,
+            data: data.into_iter().map(Ok).collect(),
         })
     }
-}
 
-pub trait Allocate {
-    type Data: Send + Clone;
-    type Item;
-    type Error;
-    fn ship(&self, data: &Self::Data) -> BoxFuture<Self::Item, Self::Error>;
+    fn ship(&self, initializer: T) -> BoxFuture<T, io::Error> {
+        let mut stream = futures::stream::iter(self.data).map(|x| {
+            self.pool.spawn_fn(|| {
+                let res: Result<T, ()> = Ok(x);
+                res
+            })
+        });
+
+        let res = stream.and_then(|x| x.wait()).collect().wait().map(|completed| {
+            completed.into_iter().fold(initializer, |a, b| a + b)
+        }).map_err(|e| io::Error::new(io::ErrorKind::Other, "failure running thread jobs"));
+        return future::result(res).boxed()
+    }
 }
--- mapreduce/src/main.rs
+++ mapreduce/src/main.rs
@@ -3,22 +3,12 @@ extern crate futures;
 
 use std::io;
 use futures::future::{Future, BoxFuture};
-use futures::future;
 
 
-impl<T> lib::Allocate for lib::Delegator<T>
-    where T: Send + Clone
-{
-    type Data = T;
-    type Item = u32;
-    type Error = io::Error;
-    fn ship(&self, data: &Self::Data) -> BoxFuture<Self::Item, Self::Error> {
-        let res = self.pool.spawn_fn(|| {
-            Ok(1)
-        }).map(|x| future::ok(x));
 
-        Box::new(res)
-    }
+fn main() {
+    let data = vec![0..10];
+    let ctrl = lib::Delegator::new(data);
+
+
 }
-
-fn main() {}
