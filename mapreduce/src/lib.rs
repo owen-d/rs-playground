56dc8ba2f309e7f0a8f179666e709275bc6e6fe3
owen-d <ow.diehl@gmail.com>
2017-05-24 16:56:02 -0400

    [wip] mpsc stream

--- mapreduce/src/lib.rs
+++ mapreduce/src/lib.rs
@@ -1,16 +1,15 @@
 extern crate futures;
 extern crate futures_cpupool;
+extern crate tokio_core;
 
-use self::futures::{future, Future, Stream};
+use self::futures::{future, Future, Stream, Sink};
 use self::future::BoxFuture;
-// use futures::sync::mpsc;
-use self::futures_cpupool::CpuPool;
-use std::io;
-use std::ops;
+use self::futures::sync::mpsc;
+use self::tokio_core::reactor::Core;
+use std::{ops, thread, io};
 
 pub struct Delegator<T> {
-    pub pool: CpuPool,
-    pub data: Option<Vec<Result<T, ()>>>,
+    data: Option<Vec<Result<T, ()>>>,
 }
 
 impl<T> Delegator<T>
@@ -18,25 +17,55 @@ impl<T> Delegator<T>
 {
     pub fn new(data: Vec<T>) -> io::Result<Self> {
         Ok(Delegator {
-            pool: CpuPool::new_num_cpus(),
             data: Some(data.into_iter().map(Ok).collect()),
         })
     }
 
     pub fn ship(&mut self, initializer: T) -> BoxFuture<T, io::Error> {
         let data = self.data.take().expect("must have data to process");
-        let stream = futures::stream::iter(data).map(|x| {
-            self.pool.spawn_fn(move || {
-                let res: Result<T, ()> = Ok(x);
-                res
+        let (tx, rx) = mpsc::channel(1);
+        let mut core = Core::new().expect("failed to create core");
+        let remote = core.remote();
+
+        let _stream = futures::stream::iter(data).map(move |x| {
+            let tx = tx.clone();
+            let remote = remote.clone();
+            thread::spawn(move || {
+
+                remote.spawn(|_| {
+                    let res: Result<T, ()> = Ok(x);
+                    tx
+                        .send(res)
+                        .then(|tx| {
+                            match tx {
+                                Ok(_tx) => {
+                                    println!("Sink flushed");
+                                    Ok(())
+                                }
+                                Err(e) => {
+                                    println!("Sink failed! {:?}", e);
+                                    Err(())
+                                }
+                            }
+                        })
+                })
             })
         });
 
-        let res = stream.and_then(|x| x.wait())
-            .collect()
-            .wait()
-            .map(|completed| completed.into_iter().fold(initializer, |a, b| a + b))
+
+        let res = rx
+            .fold(initializer, |a, b| {
+                Ok(a + b.unwrap())
+            })
             .map_err(|_| io::Error::new(io::ErrorKind::Other, "failure running thread jobs"));
-        return future::result(res).boxed();
+
+        future::result(core.run(res)).boxed()
+
+        // let res = rx
+        //     .collect()
+        //     .wait()
+        //     .map(|completed| completed.into_iter().fold(initializer, |a, b| a + b))
+        //     .map_err(|_| io::Error::new(io::ErrorKind::Other, "failure running thread jobs"));
+        // return future::result(res).boxed();
     }
 }
--- mapreduce/src/main.rs
+++ mapreduce/src/main.rs
@@ -4,9 +4,8 @@ extern crate futures;
 use futures::future::Future;
 
 
-
 fn main() {
-    let data: Vec<u64> = (0..100000).collect();
+    let data: Vec<u32> = (0..100).collect();
     let mut ctrl = lib::Delegator::new(data).unwrap();
     let res = ctrl.ship(0).wait();
 
